9c7922bc0de6c9ec62eb1500b0c56ad97bda0c74
TahaRabbani <tahabinrabbani@gmail.com>
2024-11-23 16:53:08 +0000

    adding pipeline

--- Rabbani_Site/views.py
+++ Rabbani_Site/views.py
@@ -2,23 +2,28 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse ('''<h1>Real Estate</h1> <a href= "https://www.zameen.com/Property/karachi_mehmoodabad_flat_for_sale_1st_floor_brand_new-51195832-9201-1.html"> Beautifull flat at mehmoodabad karachi</a>''')
+    return HttpResponse ('''<h1>Real Estate</h1> <a href= "https://www.zameen.com/Property/karachi_mehmoodabad_flat_for_sale_1st_floor_brand_new-51195832-9201-1.html"> Beautifull flat at mehmoodabad karachi</a>
+                        <a href= "http://127.0.0.1:8000/removepunc"> 2 bedroom flat</a>
+                        <a href= "http://127.0.0.1:8000/capitalizefirst"> 3 bedroom flat</a>
+                        <a href= "http://127.0.0.1:8000/newlineremove"> 4 bedroom flat</a>
+                        <a href= "http://127.0.0.1:8000/spaceremove"> Furnished flat</a>
+                        <a href= "http://127.0.0.1:8000/charcount"> 2 story mansion</a>''')
 
 # def about(request):
 #     return HttpResponse ("about real estate")
 
 def removepunc(request):
-    return HttpResponse ("remove punc")
+    return HttpResponse ("2 bedroom flat")
 
 def capfirst(request):
-    return HttpResponse ("capitalizefirst")
+    return HttpResponse ("3 bedroom flat")
 
 def newlineremove(request):
-    return HttpResponse ("newlineremove")
+    return HttpResponse ("4 bedroom flat")
 
 def spaceremove(request):
-    return HttpResponse ("spaceremove")
+    return HttpResponse ("Furnished flat")
 
 def charcount(request):
-    return HttpResponse ("charcount")
+    return HttpResponse ("2 story mansion")
     
