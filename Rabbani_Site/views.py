a62301a1e2bb042c155de0b5f2ff72ef7f4f4c89
TahaRabbani <tahabinrabbani@gmail.com>
2024-11-23 16:02:10 +0000

    personal navigator

--- Rabbani_Site/views.py
+++ Rabbani_Site/views.py
@@ -2,7 +2,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse ("<h1>Real Estate</h1>")
+    return HttpResponse ('''<h1>Real Estate</h1> <a href= "https://www.zameen.com/Property/karachi_mehmoodabad_flat_for_sale_1st_floor_brand_new-51195832-9201-1.html"> Beautifull flat at mehmoodabad karachi</a>''')
 
 def about(request):
     return HttpResponse ("about real estate")
